028ef5816a8d819499a6f08a0c261cf95ae1f9d5
duodec1m <47816715+duodec1m@users.noreply.github.com>
2020-05-01 16:27:47 -0700

    Implemented new grid and coordinates

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -14,13 +14,92 @@ function draw_grid( rminor, rmajor, rstroke, rfill  )
 		}
     }
 	
-	fill('gray');
-	rect(20*rminor,20*rminor,rminor,rminor); //bucket
+	fill('red');
+	rect(19*rminor,19*rminor,rminor,rminor); //bucket
 	
 	fill('green');
-	rect(30*rminor,30*rminor,rminor,rminor); //plants
-	rect(25*rminor,30*rminor,rminor,rminor);
-	rect(20*rminor,30*rminor,rminor,rminor);
-	rect(15*rminor,30*rminor,rminor,rminor);
-	rect(10*rminor,30*rminor,rminor,rminor);
+	rect(4*rminor,4*rminor,rminor,rminor); //plants
+	rect(25*rminor,4*rminor,rminor,rminor);
+	rect(31*rminor,4*rminor,rminor,rminor);
+	rect(34*rminor,7*rminor,rminor,rminor);
+	rect(28*rminor,7*rminor,rminor,rminor);
+	rect(22*rminor,7*rminor,rminor,rminor);
+	rect(15*rminor,7*rminor,rminor,rminor);
+	rect(25*rminor,10*rminor,rminor,rminor);
+	rect(31*rminor,10*rminor,rminor,rminor);
+	rect(34*rminor,13*rminor,rminor,rminor);
+	rect(28*rminor,13*rminor,rminor,rminor);
+	rect(25*rminor,13*rminor,rminor,rminor);
+	rect(10*rminor,13*rminor,rminor,rminor);
+	rect(10*rminor,16*rminor,rminor,rminor);
+	rect(7*rminor,16*rminor,rminor,rminor);
+	rect(25*rminor,16*rminor,rminor,rminor);
+	rect(28*rminor,16*rminor,rminor,rminor);
+	rect(31*rminor,16*rminor,rminor,rminor);
+	rect(34*rminor,19*rminor,rminor,rminor);
+	rect(28*rminor,19*rminor,rminor,rminor);
+	rect(25*rminor,19*rminor,rminor,rminor);
+	rect(13*rminor,19*rminor,rminor,rminor);
+	rect(10*rminor,19*rminor,rminor,rminor);
+	rect(4*rminor,19*rminor,rminor,rminor);
+	rect(7*rminor,22*rminor,rminor,rminor);
+	rect(10*rminor,22*rminor,rminor,rminor);
+	rect(13*rminor,22*rminor,rminor,rminor);
+	rect(25*rminor,22*rminor,rminor,rminor);
+	rect(28*rminor,22*rminor,rminor,rminor);
+	rect(31*rminor,22*rminor,rminor,rminor);
+	rect(4*rminor,25*rminor,rminor,rminor);
+	rect(10*rminor,25*rminor,rminor,rminor);
+	rect(13*rminor,25*rminor,rminor,rminor);
+	rect(16*rminor,25*rminor,rminor,rminor);
+	rect(19*rminor,25*rminor,rminor,rminor);
+	rect(22*rminor,25*rminor,rminor,rminor);
+	rect(25*rminor,25*rminor,rminor,rminor);
+	rect(28*rminor,25*rminor,rminor,rminor);
+	rect(34*rminor,25*rminor,rminor,rminor);
+	rect(7*rminor,28*rminor,rminor,rminor);
+	rect(10*rminor,28*rminor,rminor,rminor);
+	rect(13*rminor,28*rminor,rminor,rminor);
+	rect(16*rminor,28*rminor,rminor,rminor);
+	rect(19*rminor,28*rminor,rminor,rminor);
+	rect(22*rminor,28*rminor,rminor,rminor);
+	rect(25*rminor,28*rminor,rminor,rminor);
+	rect(28*rminor,28*rminor,rminor,rminor);
+	rect(31*rminor,28*rminor,rminor,rminor);
+	rect(4*rminor,31*rminor,rminor,rminor);
+	rect(10*rminor,31*rminor,rminor,rminor);
+	rect(16*rminor,31*rminor,rminor,rminor);
+	rect(22*rminor,31*rminor,rminor,rminor);
+	rect(28*rminor,31*rminor,rminor,rminor);
+	rect(34*rminor,31*rminor,rminor,rminor);
+	rect(7*rminor,34*rminor,rminor,rminor);
+	rect(13*rminor,34*rminor,rminor,rminor);
+	rect(19*rminor,34*rminor,rminor,rminor);
+	rect(25*rminor,34*rminor,rminor,rminor);
+	rect(31*rminor,34*rminor,rminor,rminor);
+	
+	fill('black');
+	for(var i = 14; i < 19; i++){ //barn
+		for(var j = 11; j < 15; j++){
+    	rect(i*rminor,j*rminor,rminor,rminor);
+		}
+    }
+	
+	fill('blue');
+	var j = 15;
+	for(var i = 1; i < 16; i++){ //river
+    	rect(i*rminor,j*rminor,rminor,rminor);
+		j--;
+    }
+	j = 14;
+	for(var i = 1; i < 16; i++){ 
+    	rect(i*rminor,j*rminor,rminor,rminor);
+		j--;
+    }
+	fill(rfill);
+	rect(15*rminor,0*rminor,rminor,rminor);
+	rect(5*rminor,10*rminor,rminor,rminor);
+	rect(6*rminor,10*rminor,rminor,rminor);
+	rect(11*rminor,5*rminor,rminor,rminor);
+	rect(10*rminor,5*rminor,rminor,rminor);
 }
